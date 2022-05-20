33278e3ab1f1ec57a73b75826b5ac6ec3f90b526
Steven Andrews <sandrews@ctps.org>
2022-05-20 11:17:48 -0400

    update to native pipe |>, remove rel ref

--- scape_meeting.R
+++ scape_meeting.R
@@ -7,9 +7,9 @@ library(xml2)
 webpage <- read_html("https://www.mbta.com/about/event-list?preview=&vid=latest&nid=5847")
 
 # Find the list of meetings. They seem to be in the "u-linked-card" class.
-meetings <- webpage %>% 
-  rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//div[contains(@class, 'u-linked-card')]") %>% 
+meetings <- webpage |> 
+  rvest::html_nodes('body') |> 
+  xml2::xml_find_all("//div[contains(@class, 'u-linked-card')]") |> 
   rvest::html_text()
 
 # separate the data into columns. 
@@ -17,16 +17,16 @@ meetings <- webpage %>%
 meetings_df <- meetings |>
   as_tibble() |> 
   separate(col = "value", 
-           into = c("MeetingName","Date_Time","blank","MeetingName_dupe", "blank2", "Location", "blank3"), 
+           into = c("meeting_name","date_time","blank","meeting_name_dupe", "blank2", "location", "blank3"), 
            sep = "  " ) |> 
-  select(-blank, -blank2, -blank3, -MeetingName_dupe) |> 
-  mutate_all(trimws)
+  select(-blank, -blank2, -blank3, -meeting_name_dupe) |> 
+  mutate_all(trimws) # clean up /n charachters
 
 # grab the hrefs. These didn't come with the original search, they're an attribute of the first meeting name?
 # The hrefs seem to be relative to the website domain "www.mbta.com"
-link <- webpage %>% 
-  rvest::html_nodes('body') %>% 
-  xml2::xml_find_all("//a[contains(@class, 'u-linked-card__primary-link')]") %>% 
+link <- webpage |> 
+  rvest::html_nodes('body') |> 
+  xml2::xml_find_all("//a[contains(@class, 'u-linked-card__primary-link')]") |> 
   rvest::html_attr('href')
 
 # make the links into a dataframe and rebuild the links.
@@ -34,10 +34,10 @@ link_df <- link |>
   as_tibble() |> 
   mutate(href_rel = value,
          href_full = paste0("www.mbta.com", href_rel)) |> 
-  select(-value)
+  select(-value, -href_rel)
 
 # bind the two datasets together
 scraped_site <- bind_cols(meetings_df, link_df)
 
 # export the data
-write_csv(scraped_site, "./triennial-audits_MBTA-events_Jan20-May22.csv")
+write_csv(scraped_site, "./triennial-audits_MBTA-events_Jan2020-May2022.csv")
